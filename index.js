b02d2c6c997bd766abef0338fb330163f53a8819
platofff <42435266+platofff@users.noreply.github.com>
2021-05-14 09:51:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,15 @@ firebase.initializeApp(firebaseConfig)
 const onlineDiv = document.getElementById('online')
 const adminsDiv = document.getElementById('admins')
 const membersCount = document.getElementById('members-count')
+const getURL = (vk_id) => {
+  return vk_id > 0 ? `https://vk.com/id${vk_id}` : `https://vk.com/club${Math.abs(vk_id)}`
+}
 
 firebase.database().ref('online').on('value', (snapshot) => {
   const online = snapshot.val()
   onlineDiv.innerHTML = ''
   for (const member of Object.keys(online)) {
-    onlineDiv.innerHTML += `<div onclick="window.open('https://vk.com/id${member}', '_blank')" class="member">
+    onlineDiv.innerHTML += `<div onclick="window.open('${getURL(member)}', '_blank')" class="member">
       <img src="${online[member][1]}">
       ${online[member][0]}
     </div>`
@@ -31,7 +34,7 @@ firebase.database().ref('admins').on('value', (snapshot) => {
   const admins = snapshot.val()
   adminsDiv.innerHTML = ''
   for (const member of Object.keys(admins)) {
-    adminsDiv.innerHTML += `<div onclick="window.open('https://vk.com/id${member}', '_blank')" class="member">
+    adminsDiv.innerHTML += `<div onclick="window.open('${getURL(member)}', '_blank')" class="member">
         <img src="${admins[member][1]}">
         ${admins[member][0]}
       </div>`
